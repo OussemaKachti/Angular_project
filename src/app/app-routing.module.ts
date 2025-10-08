59b7a8ebbdc4ed34acdd0361ae956099d555ca9f
Oussema Kachti <oussema.kachti@esprit.tn>
2025-10-08 10:39:48 +0200

    feat: initial project setup with Angular CLI and starter structure

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './layout/not-found/not-found.component';
+import { LidtEventComponent } from './layout/list-event/list-event.component';
+import { HomeComponent } from './layout/home/home.component';
+
+const routes: Routes = [
+  {path:'home' , component:HomeComponent},
+  {path:'list' , component:LidtEventComponent,children:[
+      {path:'test' , component:HomeComponent}
+  ]},
+  {path:'**' , component:NotFoundComponent}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
