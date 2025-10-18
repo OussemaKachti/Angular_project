97fc5ab7769dc5c95f9739578405197ef7ed55e5
Oussema Kachti <oussema.kachti@esprit.tn>
2025-10-18 12:46:21 +0100

    feat: implement event details functionality with service integration

--- src/app/features/events/events-routing.module.ts
+++ src/app/features/events/events-routing.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EventsComponent } from './events.component';
 import { ListEventComponent } from './list-event/list-event.component';
+import { EventDetailsComponent } from './event-details/event-details.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'list', pathMatch: 'full' },
+  { path: '', component: EventsComponent,
+    children: [
+      {path:'',component:ListEventComponent},
+      {path: ':id',component: EventDetailsComponent},
+    ]
+   },
   { path: 'list', component: ListEventComponent },
   { path: '**', component: EventsComponent }
 ];
--- src/app/features/events/events.module.ts
+++ src/app/features/events/events.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { EventsRoutingModule } from './events-routing.module';
 import { EventsComponent } from './events.component';
 import { ListEventComponent } from './list-event/list-event.component';
 import { FormsModule } from '@angular/forms';
+import { EventDetailsComponent } from './event-details/event-details.component';
 
 
 @NgModule({
   declarations: [
     EventsComponent,
-    ListEventComponent
+    ListEventComponent,
+    EventDetailsComponent
   ],
   imports: [
     CommonModule,
     EventsRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule
   ]
 })
 export class EventsModule { }
